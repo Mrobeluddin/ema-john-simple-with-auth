57d8409174801b0c7c748cfc550e98d3453d04c7
Mrobeluddin <mrobel385@gmail.com>
2022-04-12 03:08:55 +0600

    note

--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -16,10 +16,11 @@
 
 
 /**
- * hosting steps
- * 1. npm install -g firebase-tools
- * 2  firebase login
- * 3. firebase init
- * 4. firebase deploy
+ * Firebase hosting steps
+ * 1. npm install -g firebase-tools(one time for you computer)
+ * 2  firebase login(one time for you computer)
+ * 3. firebase init(one time for each project)
+ * 4. npm run build (every time you want to deploy)
+ * 5. firebase deploy(every time you want to deploy)
  * 
  * */ 
